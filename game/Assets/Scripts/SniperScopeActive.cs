7c1f99237794cd329e797daeb70db642ab10f05f
pminhtam <pminhtamnb@gmail.com>
2019-04-30 15:35:50 +0700

    34,35

---
+++ game/Assets/Scripts/SniperScopeActive.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperScopeActive : MonoBehaviour
+{
+    public GameObject playerCam;
+    public GameObject sniperScopeTex;
+    public GameObject originalCursor;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            playerCam.GetComponent<Camera>().fieldOfView = 25;
+            sniperScopeTex.SetActive(true);
+            originalCursor.SetActive(false);
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            playerCam.GetComponent<Camera>().fieldOfView = 60;
+            sniperScopeTex.SetActive(false);
+            originalCursor.SetActive(true);
+        }
+    }
+}
